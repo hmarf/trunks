ef9eb0a8ef1e5ed8e7d0a3ab6f411acf1b5574ac
hmarf <hmarfUni@gmail.com>
2019-11-17 15:01:43 +0900

    create channel

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -11,66 +12,49 @@ type RequestResult struct {
 	ResponseTime string
 }
 
-func Hello(wg *sync.WaitGroup, ch *chan int, re chan RequestResult) {
-	// defer wg.Done()
-	// fmt.Println("hello world")
-	// re <- RequestResult{
-	// 	StatusCode:   200,
-	// 	ResponseTime: "time",
-	// }
-	// <-*ch
-
+func Hello(wg *sync.WaitGroup, ch *chan int, client *http.Client,
+	req *http.Request, re chan RequestResult) {
+	fmt.Println("aa")
+	time.Sleep(time.Second)
 	defer wg.Done()
 	rqStart := time.Now()
 	resp, _ := client.Do(req)
 	rqEnd := time.Now()
-	result[i] = RequestResult{
+	re <- RequestResult{
 		StatusCode:   resp.StatusCode,
 		ResponseTime: rqEnd.Sub(rqStart).String(),
 	}
+	<-*ch
 }
 
 func main() {
-	// 並行処理するスレッドの個数を決める
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:8080", nil)
+
+	// 並行処理するスレッド数を決める
 	ch := make(chan int, 2)
+
 	//
-	result_ch := make(chan RequestResult, 2)
+	result_ch := make(chan RequestResult, 2000)
 
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 10; i++ {
 		ch <- 1
 		wg.Add(1)
-		go Hello(&wg, &ch, result_ch)
-		fmt.Println(<-result_ch)
+		go Hello(&wg, &ch, client, req, result_ch)
 	}
+
 	wg.Wait()
+
+LOOP:
+	for {
+		select {
+		case aa := <-result_ch:
+			fmt.Println(aa)
+		default:
+			break LOOP
+		}
+	}
+
 }
-
-// func Attack(ch chan []RequestResult, count int, client *http.Client, req *http.Request) {
-// 	result := make([]RequestResult, count)
-// 	for i := 0; i < count; i++ {
-// 		rqStart := time.Now()
-// 		resp, _ := client.Do(req)
-// 		rqEnd := time.Now()
-// 		result[i] = RequestResult{
-// 			StatusCode:   resp.StatusCode,
-// 			ResponseTime: rqEnd.Sub(rqStart).String(),
-// 		}
-// 		fmt.Println(resp.StatusCode)
-// 	}
-// 	ch <- result
-// }
-
-// func main() {
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
-// 	if err != nil {
-// 	}
-// 	for i := 0; i < 3; i++ {
-// 		txtCh := make(chan []RequestResult, 200)
-// 		go Attack(txtCh, 2, client, req)
-// 		fmt.Println(<-txtCh)
-// 	}
-// 	time.Sleep(3 * time.Second)
-// }
