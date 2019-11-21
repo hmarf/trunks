c9ec98e81990ce16bbe785c29f40bc840b0d1682
hmarf <hmarfUni@gmail.com>
2019-11-21 14:13:19 +0900

    [add] convert for json

--- report/report.go
+++ report/report.go
@@ -24,13 +24,45 @@ type ResultBenchMark struct {
 	LatecyAve         time.Duration `json:"latecy_ave"`          // Responseが来る待機時間の平均
 }
 
+// ベンチマークの結果を計算し収納する場所
+type ResultBenchMarkForJson struct {
+	ConcurrencyLevel  int         `json:"concurrency_level"`   //
+	TotalRequests     int         `json:"total_requests"`      //
+	SucceedRequests   int         `json:"succeed"`             // 通信に成功したRequest
+	FailedRequests    int         `json:"failed"`              // 何らかの理由で通信に失敗したRequest
+	RequestsSec       int         `json:"requests_sec"`        // 一秒間にアクセスできたRequestの総数
+	TotalDataReceived int64       `json:"total_data_reveived"` // ContentLengthの総数
+	StatusCode        map[int]int `json:"status_code"`         // サーバーから返ってきたStatusCode
+	LatecyTotal       string      `json:"latecy_total"`        // 全てのResponseが返ってくるまでの総時間
+	LatecyMax         string      `json:"latecy_max"`          // Responseが来る待機時間の最も長かったもの
+	LatecyMin         string      `json:"latecy_min"`          // Responseが来る待機時間の最も短かったもの
+	LatecyAve         string      `json:"latecy_ave"`          // Responseが来る待機時間の平均
+}
+
 func (r *ResultBenchMark) ShowResult() {
 	r.WriteResultFile()
 	r.ShowResultConsole()
 }
 
+func convertStringFromDuration(r ResultBenchMark) ResultBenchMarkForJson {
+	return ResultBenchMarkForJson{
+		ConcurrencyLevel:  r.ConcurrencyLevel,
+		TotalRequests:     r.TotalRequests,
+		SucceedRequests:   r.SucceedRequests,
+		FailedRequests:    r.FailedRequests,
+		RequestsSec:       r.RequestsSec,
+		TotalDataReceived: r.TotalDataReceived,
+		StatusCode:        r.StatusCode,
+		LatecyTotal:       fmt.Sprintf("%v", r.LatecyTotal),
+		LatecyMax:         fmt.Sprintf("%v", r.LatecyMax),
+		LatecyMin:         fmt.Sprintf("%v", r.LatecyMin),
+		LatecyAve:         fmt.Sprintf("%v", r.LatecyAve),
+	}
+}
+
 func (r *ResultBenchMark) WriteResultFile() {
-	jsonBytes, err := json.Marshal(*r)
+	resultJson := convertStringFromDuration(*r)
+	jsonBytes, err := json.Marshal(resultJson)
 	if err != nil {
 		log.Println("ベンチマークの結果の保存に失敗しました(json文字列の作成に失敗しました)")
 		return
